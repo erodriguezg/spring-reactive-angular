dd992beb2facf8b3924880de7b88b73f4161ed81
Eduardo Rodriguez <eduardo.rodriguez@zeke.cl>
2018-01-03 20:45:14 -0300

    limpieza

--- src/main/java/com/github/erodriguezgarq/springreactiveangular/config/WebSecurityConfig.java
+++ src/main/java/com/github/erodriguezgarq/springreactiveangular/config/WebSecurityConfig.java
@@ -5,8 +5,6 @@ import com.github.erodriguezg.security.jwt.TokenAuthenticationHttpHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
