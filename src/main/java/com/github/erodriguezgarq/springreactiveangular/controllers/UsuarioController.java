9008890e4f32f5ead3d3aa81a9f39c2ad16193ca
erodriguezg <erodriguez.cl@gmail.com>
2018-01-04 11:53:44 -0300

    limpieza

--- src/main/java/com/github/erodriguezgarq/springreactiveangular/controllers/UsuarioController.java
+++ src/main/java/com/github/erodriguezgarq/springreactiveangular/controllers/UsuarioController.java
@@ -9,11 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * @author eduardo
@@ -32,18 +39,24 @@ public class UsuarioController {
     public Mono<ResponseEntity<UsuarioDto>> traerPorId(@PathVariable("username") String username) {
         return usuarioService.traerPorUsernameConPerfilYPersona(username)
                 .map(usuarioDto -> new ResponseEntity<>(usuarioDto, HttpStatus.OK))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .onErrorReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
-    @RequestMapping(value = "/buscar", method = RequestMethod.POST)
-    public List<UsuarioDto> buscar(@RequestBody UsuarioFiltroDto filtros) {
-        log.debug("-> buscar");
-        return null;
+    @PostMapping("/buscar")
+    public Flux<ResponseEntity<UsuarioDto>> buscar(@RequestBody UsuarioFiltroDto filtros) {
+        log.debug("-> buscando usuarios por filtros: {}", filtros);
+        return usuarioService.buscar(filtros)
+                .map(usuarioDto -> new ResponseEntity<>(usuarioDto, HttpStatus.OK))
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                .onErrorReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
     @RequestMapping(value = "/guardar", method = RequestMethod.POST)
-    public void guardar(@RequestBody @Valid UsuarioDto usuario) {
+    public Mono<ResponseEntity<Void>> guardar(@RequestBody @Valid UsuarioDto usuario) {
         log.debug("vacio");
+        return Mono.just(ResponseEntity.ok().<Void>build())
+                .onErrorReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).<Void>build());
     }
 
 }
